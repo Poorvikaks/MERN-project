d19a3fd4703584fc8721a85c8ae3b611f8d31c4f
poorvika_ks <poorvika@gmail.com>
2026-01-16 13:43:56 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`SERVER IS RUNNING IN THE PORT: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed ", err);
+  });
 
 // (async()=>{
 //     try {
